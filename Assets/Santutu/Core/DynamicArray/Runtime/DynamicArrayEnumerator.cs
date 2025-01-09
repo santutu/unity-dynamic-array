1072ef0d18c29adc7f4f5990cb502a1bf694bead
santutu <santutu@naver.com>
2025-01-09 15:40:38 +0900

    added package.json

--- Assets/Santutu/Core/DynamicArray/Runtime/DynamicArrayEnumerator.cs
+++ Assets/Santutu/Core/DynamicArray/Runtime/DynamicArrayEnumerator.cs
@@ -5,20 +5,20 @@ namespace Santutu.Core.DynamicArray.Runtime
 {
     public struct DynamicArrayEnumerator<T> : IEnumerator<T>
     {
-        private DynamicArray<T> _colliderReceiver;
+        private readonly DynamicArray<T> _array;
 
         private int _current;
 
-        public DynamicArrayEnumerator(DynamicArray<T> colliderReceiver)
+        public DynamicArrayEnumerator(DynamicArray<T> array)
         {
-            _colliderReceiver = colliderReceiver;
+            _array = array;
             _current = -1;
         }
 
         public bool MoveNext()
         {
             _current += 1;
-            return _current < _colliderReceiver.Length;
+            return _current < _array.Length;
         }
 
         public void Reset()
@@ -26,7 +26,7 @@ namespace Santutu.Core.DynamicArray.Runtime
             _current = -1;
         }
 
-        public T Current => _colliderReceiver.items[_current];
+        public T Current => _array.items[_current];
 
         object IEnumerator.Current => Current;
 
